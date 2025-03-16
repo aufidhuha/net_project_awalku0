6eb3e541f6e8ccc3427a2777c63eac504b52f66c
ASUS <aufikawanit.com>
2025-03-16 08:28:34 +0700

    first commit

---
+++ src/projectku01/ProjectKu01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectku01;
+
+/*
+    Nama  : Aufi Dhuha Mutaafif
+    NIM   : 202457201015
+    Prodi : Sistem Informasi - A
+*/
+
+public class ProjectKu01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello Aufi");
+        System.out.println("Hello Dhuha");
+        System.out.println("Hello Mutaafif");
+    }
+    
+}
